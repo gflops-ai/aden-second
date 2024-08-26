66d17a105d2e59eb4a858c1bc88b58d60ef8e91f
maritaro <157350086+maritaro@users.noreply.github.com>
2024-08-27 00:55:08 +0900

    css

--- src/components/TypingBubble.tsx
+++ src/components/TypingBubble.tsx
@@ -6,21 +6,14 @@
   </div>
 ); 元のコード変更したよ*/
 
-/*
-export const TypingBubble = () => (
-  <div class="flex items-center">
-    <span class="mr-2">回答生成中</span>
-    <div class="w-2 h-2 mr-1 rounded-full bubble1" />
-    <div class="w-2 h-2 mr-1 rounded-full bubble2" />
-    <div class="w-2 h-2 rounded-full bubble3" />
-  </div>
-); テキストを追加したバージョン*/
 
 export const TypingBubble = () => (
   <div class="flex items-center">
     <span class="mr-2">回答生成中</span>
-    <div class="w-2 h-2 mr-1 rounded-full bubble1" style={{ border: '1px solid black' }} />
-    <div class="w-2 h-2 mr-1 rounded-full bubble2" style={{ border: '1px solid black' }} />
-    <div class="w-2 h-2 rounded-full bubble3" style={{ border: '1px solid black' }} />
+    <div class="w-1 h-1 mr-0.5 rounded-full bubble1" />
+    <div class="w-1 h-1 mr-0.5 rounded-full bubble2" />
+    <div class="w-1 h-1 rounded-full bubble3" />
   </div>
-);
+); 
+
+
