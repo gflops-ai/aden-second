8117672ac7c35d7bf5f4beb9fa3720dbf1767779
maritaro <157350086+maritaro@users.noreply.github.com>
2024-08-28 15:31:07 +0900

    thumsup

--- src/components/icons/ThumbsUpIcon.tsx
+++ src/components/icons/ThumbsUpIcon.tsx
@@ -46,7 +46,7 @@ export const ThumbsUpIcon = (props: JSX.SvgSVGAttributes<SVGSVGElement>) => (
     display: 'flex',
     'align-items' : 'center', // アイコンとテキストを中央揃いにする
     gap: '4px' // アイコンとテキストの間にスペースを追加（必要に応じて調整）
-    "font-weight": 300 // テキストを細くする
+
   }}
   >
     <svg
@@ -61,7 +61,7 @@ export const ThumbsUpIcon = (props: JSX.SvgSVGAttributes<SVGSVGElement>) => (
         fill="black"
       />
     </svg>
-    良い回答
+    <span style={{ 'font-weight': 100 }}> 良い回答</span>
   </span>
 );
 
